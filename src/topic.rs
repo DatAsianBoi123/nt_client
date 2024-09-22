64f41888cfb02f68719077213400e0f61ff8d8bf
Zachary Su <zach.su158@gmail.com>
2024-09-21 23:33:01 -0600

    created TopicPath struct and path! macro

--- src/topic.rs
+++ src/topic.rs
@@ -2,11 +2,44 @@
 //!
 //! Topics have a fixed data type and can be subscribed and published to.
 
-use std::{collections::HashMap, sync::Arc};
+use std::{collections::{HashMap, VecDeque}, fmt::Display, ops::{Deref, DerefMut}, sync::Arc};
 
 use tokio::sync::RwLock;
 
 use crate::{data::{r#type::{DataType, NetworkTableData}, Announce, Properties, SubscriptionOptions}, publish::{NewPublisherError, Publisher}, subscribe::Subscriber, NTClientSender, NTServerSender, NetworkTablesTime};
+
+#[macro_export]
+/// Creates a [`TopicPath`] containing the segments.
+///
+/// `path!` allows for the easy creation of [`TopicPath`]s without having to deal with creating
+/// [`VecDeque`]s.
+/// Its usage is extremely similar to [`std::vec!`].
+///
+/// # Examples
+/// ```
+/// use std::collections::VecDeque;
+/// use nt_client::{topic::TopicPath, path};
+///
+/// let mut vec_deque = VecDeque::new();
+/// vec_deque.push_back("my".to_owned());
+/// vec_deque.push_back("path".to_owned());
+/// let path = TopicPath::new(vec_deque);
+/// 
+/// assert_eq!(path!["my", "path"], path);
+/// ```
+macro_rules! path {
+    () => {
+        $crate::topic::TopicPath::default();
+    };
+
+    ($($segment: literal),+ $(,)?) => {{
+        let mut segments = std::collections::VecDeque::new();
+        $(
+            segments.push_back($segment.to_string());
+        )*
+        $crate::topic::TopicPath::new(segments)
+    }};
+}
 
 /// Represents a `NetworkTables` topic.
 ///
@@ -120,6 +153,164 @@ impl From<&Announce> for AnnouncedTopic {
             r#type: value.r#type.clone(),
             properties: value.properties.clone(),
         }
+    }
+}
+
+/// Represents a slash (`/`) deliminated path.
+///
+/// This is especially useful when trying to parse nested data, such as from Shuffleboard
+/// (found in `/Shuffleboard/...`).
+///
+/// This can be thought of as a wrapper for a [`VecDeque`], only providing trait impls to convert
+/// to/from a [`String`].
+///
+/// # Note
+/// The [`Display`] impl will always contain a leading slash, but not a trailing one,
+/// regardless of if the path was parsed from a [`String`] containing either a leading or trailing
+/// slash.
+///
+/// # Warning
+/// In cases where slashes are present in segment names, turning to and from a [`String`] is
+/// **NOT** guaranteed to preserve segment names.
+///
+/// ```should_panic
+/// use nt_client::{topic::TopicPath, path};
+///
+/// let path = path!["///weird//", "na//mes//"];
+///
+/// // this will panic!
+/// assert_eq!(<String as Into<TopicPath>>::into(path.to_string()), path);
+/// ```
+///
+/// In the above example, `.to_string()` is converting the path to `////weird///na//mes//`.
+/// When turning this back into a `TopicPath`, it recognizes the following segments (with
+/// trailing and leading slashes removed):
+///
+/// **/** / **/weird** / **/** / **na** / **/mes** /
+///
+/// # Examples
+/// ```
+/// use nt_client::{topic::TopicPath, path};
+///
+/// // automatically inserts a leading slash
+/// assert_eq!(path!["my", "topic"].to_string(), "/my/topic");
+///
+/// // slashes in the segment names are preserved
+/// assert_eq!(path!["some", "/data"].to_string(), "/some//data");
+///
+/// assert_eq!(<&str as Into<TopicPath>>::into("/path/to/data"), path!["path", "to", "data"]);
+///
+/// assert_eq!(<&str as Into<TopicPath>>::into("//some///weird/path/"), path!["/some", "/", "weird", "path"]);
+/// ```
+/// Getting a topic:
+/// ```no_run
+/// use nt_client::{Client, path};
+///
+/// # tokio_test::block_on(async {
+/// let client = Client::new(Default::default());
+///
+/// let topic = client.topic(path!["nested", "data"]);
+///
+/// // do something with `topic`
+///
+/// client.connect().await;
+/// # });
+/// ```
+/// Parsing topic name:
+/// ```no_run
+/// use nt_client::{topic::TopicPath, data::SubscriptionOptions, subscribe::ReceivedMessage, Client};
+///
+/// # tokio_test::block_on(async {
+/// let client = Client::new(Default::default());
+///
+/// let sub_topic = client.topic("/Root/");
+/// tokio::spawn(async move {
+///     let mut sub = sub_topic.subscribe(SubscriptionOptions {
+///         topics_only: Some(true),
+///         prefix: Some(true),
+///         ..Default::default()
+///     }).await;
+///
+///     while let Ok(ReceivedMessage::Announced(topic)) = sub.recv().await {
+///         let path: TopicPath = topic.name().into();
+///
+///         // do something with `path`
+///     }
+/// });
+///
+/// client.connect().await;
+///
+/// # });
+/// ```
+#[derive(Default, Debug, Clone, PartialEq, Eq, Hash)]
+pub struct TopicPath {
+    segments: VecDeque<String>,
+}
+
+impl TopicPath {
+    /// The delimiter to use when converting from a [`String`].
+    pub const DELIMITER: char = '/';
+
+    /// Creates a new `TopicPath` with segments.
+    pub fn new(segments: VecDeque<String>) -> Self {
+        Self { segments }
+    }
+}
+
+impl From<VecDeque<String>> for TopicPath {
+    fn from(value: VecDeque<String>) -> Self {
+        Self { segments: value }
+    }
+}
+
+impl Deref for TopicPath {
+    type Target = VecDeque<String>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.segments
+    }
+}
+
+impl DerefMut for TopicPath {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.segments
+    }
+}
+
+impl Display for TopicPath {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let full_path = self.segments.iter().fold(String::new(), |prev, curr| prev + "/" + curr);
+        f.write_str(&full_path)
+    }
+}
+
+impl From<&str> for TopicPath {
+    fn from(value: &str) -> Self {
+        value.to_string().into()
+    }
+}
+
+impl From<String> for TopicPath {
+    fn from(value: String) -> Self {
+        // TODO: fix this horrible code
+        let str = value.strip_prefix(Self::DELIMITER)
+            .map(|str| str.to_owned())
+            .unwrap_or(value);
+        let str = str
+            .strip_suffix(Self::DELIMITER)
+            .map(|str| str.to_owned())
+            .unwrap_or(str);
+
+        str.chars().fold((VecDeque::<String>::new(), true), |(mut parts, prev_is_delimiter), char| {
+            if prev_is_delimiter {
+                parts.push_back(String::from(char));
+                (parts, false)
+            } else {
+                let is_delimiter = char == Self::DELIMITER;
+                if !is_delimiter { parts.back_mut().unwrap().push(char); };
+                (parts, is_delimiter)
+            }
+        }).0.into()
     }
 }
 
