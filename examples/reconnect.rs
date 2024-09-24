315c11da600371d5beeef52ca03cd6e9dc089055
Zachary Su <zach.su158@gmail.com>
2024-09-23 23:16:46 -0600

    updated reconnect example with more comments

--- examples/reconnect.rs
+++ examples/reconnect.rs
@@ -1,12 +1,14 @@
 //! An example of using a reconnect handler.
 
-use std::error::Error;
-
 use nt_client::{data::r#type::NetworkTableData, error::ReconnectError, subscribe::ReceivedMessage};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+async fn main() {
+    // creates a reconnect handler with the NewClientOptions of Default::default()
     nt_client::reconnect(Default::default(), |client| async {
+        // handle everything in this closure
+
+        // subscribes to topic `/value`
         let sub_topic = client.topic("/value");
         let sub_task = tokio::spawn(async move {
             let mut subscriber = sub_topic.subscribe(Default::default()).await;
@@ -23,12 +25,17 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
             }
         });
 
+        // select! to make sure tokio tasks don't keep running
         tokio::select! {
+            // fatal error if the client encounters an IoError, nonfatal otherwise
             res = client.connect() => Ok(res?),
+            // fatal error if the task encountered an error while joining,
+            // otherwise return a nonfatal error
             res = sub_task => res
                 .map_err(|err| ReconnectError::Fatal(err.into()))?
                 .map_err(|err| ReconnectError::Nonfatal(err.into())),
         }
-    }).await
+    }).await.unwrap()
+    // unwrap the fatal error (if there is one)
 }
 
